9660c58273900e124bcd2147dd732c46297e87fd
Fernanda <fernanda19@usf.edu>
2024-09-08 14:44:49 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -7,3 +7,19 @@ function calculateAverageSales (sales) {
 } // develop a function that accepts an array: sales is the array. the description gave me the input. 
 // output will be the average 
 
+// function to determine performance rating
+function determinePerformanceRating (average) {
+    let PerformanceRating
+if (average > 10000) {
+    PerformanceRating = "Excellent" //output excellent
+} else if (7000 <= average && average <= 10000) {
+   PerformanceRating = "Good" // output good
+} else if (4000 <= average && average < 7000) {
+    PerformanceRating = "Satisfactory" // output satisfactory
+} else {
+    PerformanceRating = "Needs Improvement" // output needs improvement
+} 
+return PerformanceRating 
+} 
+
+
